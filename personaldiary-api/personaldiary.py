365619c204da8806f60bbb1b56dff3abb1b45828
Sai Harsha Gangari <harshagangari747@gmail.com>
2025-02-03 23:00:06 -0500

    connected to backend and database

--- personaldiary-api/personaldiary.py
+++ personaldiary-api/personaldiary.py
@@ -1,38 +1,71 @@
 from http.client import HTTPResponse
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from pymongo import MongoClient
+from bson import ObjectId
+
 
 app = Flask(__name__)
 
-# Enable CORS for the entire app, allowing only the React app's origin
 CORS(app,resources={r"/*":{
-    "origins":"http://localhost:5173"
+    "origins":"http://localhost:5173/",
+    "methods":["GET","POST","OPTIONS"],
+    "allow-headers":["Content-Type","Authorization"]
     }})
 
-client = MongoClient("mongodb://127.0.0.1:27017")
+client = MongoClient("mongodb://localhost:27017")
 db = client["personaldiarydb"]
 pageCollection = db['pages']
 
+## Retrieve the most recent page written
 @app.route('/',methods=['GET'])
 def getLastWrittenPage():
     try:
         row = pageCollection.find()
-        result = str(row[0])
+        
+        result = list(row)
+
+        if result:
+            result = result[0]
+            result['_id'] = str(result['_id'])
+        print('res',result)
+
+        return jsonify(result)
+    except:
+        return HTTPResponse(400)
+    finally:
+        client.close();
+
+
+## Retrive the page at a given date
+@app.route('/page',methods=['GET'])
+def getPageAtDate():
+    date = request.args.get('date')
+    print('backend',date)
+    try:
+        row = pageCollection.find({"title.date":{"$eq":date}})
+        result = list(row)
+        if result:
+            result = result[0]
+            result['_id'] = str(result['_id'])
         print('res',result)
         return jsonify(result)
     except:
         return HTTPResponse(400)
+    finally:
+        client.close()
 
-@app.route('/',methods=["POST"])
-def writeNewPage():
-    return 'written'
+@app.route('/post',methods=["POST"])
+def postPage():
+    try:
+        data = request.get_json()
+        return None
+    except:
+        return ConnectionRefusedError()
+    finally:
+        client.close()
+    
 
-@app.route('/page',methods=['GET'])
-def getPageAtDate():
-    date = request.args.get('date');
-
-    return 'page on date'+date
 
 if __name__=='__main__':
     app.run(debug=False)
